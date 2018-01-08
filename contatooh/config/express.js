d834065ac4c5a022ad4716cc399bc48956359bd1
schiozer <schiozer@yahoo.com.br>
2018-01-07 22:10:29 -0200

    m

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -2,6 +2,7 @@
 var express = require('express');
 //var home = require('../app/routes/home'); com o express load isso aqui não é mais necessário
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function() {
     
@@ -16,6 +17,12 @@ module.exports = function() {
     app.set('view engine', 'ejs');
     app.set('views','./app/views');
 
+    /*middleware abaixo são para method override. Usando POST e fazendo overrive
+    para interpretar o DELETE e PUT */
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
+    
     // abaixo da configuração do último middleware
     //home(app);
     load('models', {cwd: 'app'})
